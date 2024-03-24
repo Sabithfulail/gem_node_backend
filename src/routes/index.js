97d06b79cba108cb686716a3294922ff16e2186a
MassInshaf <97073682+MassInshaf@users.noreply.github.com>
2024-03-24 16:49:57 +0530

    Changed the endpoint name in my branch and changed this manually in main

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,5 +9,5 @@ const url_prefix = "/api";
 router.use(`${url_prefix}/customers`, new CustomerRoutes().getRouter());
 router.use(`${url_prefix}/items`, new ItemRoutes().getRouter());
 router.use(`${url_prefix}/orders`, new OrderRoutes().getRouter);
-router.use(`${url_prefix}/Login`, new LoginRoutes().getRouter); // Mount the LoginRoutes
+router.use(`${url_prefix}/login`, new LoginRoutes().getRouter); // Mount the LoginRoutes
 module.exports = router;

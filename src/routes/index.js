7552d180c9ca4be7b00fe4d95861745b0eeae4c7
MassInshaf <97073682+MassInshaf@users.noreply.github.com>
2024-03-25 01:17:39 +0530

    edit API URL

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ const CustomerRoutes = require('./CustomerRoutes');
 const ItemRoutes = require('./ItemRoutes');
 const OrderRoutes = require('./OrderRoutes');
 const LoginRoutes = require('./LoginRoutes');  // Include the LoginRoutes
+const RegistrationRoutes = require('./RegistrationRoutes');  // Include the LoginRoutes
 
 const url_prefix = "/api";
 router.use(`${url_prefix}/customers`, new CustomerRoutes().getRouter());
@@ -11,4 +12,5 @@ router.use(`${url_prefix}/items`, new ItemRoutes().getRouter());
 router.use(`${url_prefix}/orders`, new OrderRoutes().getRouter);
 //login api created
 router.use(`${url_prefix}/logins`, new LoginRoutes().getRouter); // Mount the LoginRoutes
+router.use(`${url_prefix}/registration`, new RegistrationRoutes().getRouter); // Mount the LoginRoutes
 module.exports = router;

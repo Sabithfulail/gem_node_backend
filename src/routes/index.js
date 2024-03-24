289f4a031fcbf18184dda5ef1dfa820747a1bbf3
MassInshaf <97073682+MassInshaf@users.noreply.github.com>
2024-03-24 16:41:40 +0530

    Add API end point for the Login API

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 const CustomerRoutes = require('./CustomerRoutes');
 const ItemRoutes = require('./ItemRoutes');
 const OrderRoutes = require('./OrderRoutes');
+const LoginRoutes = require('./LoginRoutes');  // Include the LoginRoutes 
 
 const url_prefix = "/api";
 router.use(`${url_prefix}/customers`, new CustomerRoutes().getRouter());
 router.use(`${url_prefix}/items`, new ItemRoutes().getRouter());
 router.use(`${url_prefix}/orders`, new OrderRoutes().getRouter);
+router.use(`${url_prefix}/Login`, new LoginRoutes().getRouter); // Mount the LoginRoutes
 module.exports = router;

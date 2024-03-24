3e8deec5a55daba832a7e0e03b64785402eaae16
Zabith Fulail <muhammadh_s@epiclanka.net>
2024-03-24 12:38:54 +0530

    Revert "user registration api added"
    This reverts commit bfc7b2f8f30cdfc4757ab4125da02a35eb6bd5e7.

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,9 @@ const router = express.Router();
 const CustomerRoutes = require('./CustomerRoutes');
 const ItemRoutes = require('./ItemRoutes');
 const OrderRoutes = require('./OrderRoutes');
-const UserRoutes = require('./UserRoutes');
 
 const url_prefix = "/api";
 router.use(`${url_prefix}/customers`, new CustomerRoutes().getRouter());
 router.use(`${url_prefix}/items`, new ItemRoutes().getRouter());
 router.use(`${url_prefix}/orders`, new OrderRoutes().getRouter);
-router.use(`${url_prefix}/users`, UserRoutes);
 module.exports = router;
